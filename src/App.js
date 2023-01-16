a3254d4404e1b06e5cbcf575f06b55f80514ce16
Dang <haidang.tr1309@gmail.com>
2023-01-16 19:15:57 +0700

    trst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from './components/GlobalStyle'
+import Heading from './components/heading/Heading';
+import Intro from './components/intro/Intro';
+import Feature from './components/feature/Feature';
+import Review1 from './components/review1/Review1';
+import Review2 from './components/review2/Review2';
+import Background from './components/background/Background';
+import AboutUs from './components/aboutUs/AboutUs';
+import Buy from './components/buy/Buy';
+import Order from './components/order/Order';
+import Options from './components/options/Options';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 123
-        </a>
-      </header>
+    <GlobalStyle>
+      <div className="App">
+        <Heading />
+        <Intro />
+        <Feature />
+        <Review1 />
+        <Review2 />
+        <Background />
+        <AboutUs />
+        <Buy />
+        <Order />
+        <Options />
+        <Contact />
+        <Footer />
+
     </div>
+    </GlobalStyle>
   );
 }
 
 export default App;
+
+
